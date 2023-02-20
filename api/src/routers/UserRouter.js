59e2ab9ffa0022ecfed1ddf08c4a88c321c68ab9
Narayan Aryal <aryal.coding@gmail.com>
2023-02-20 12:25:57 +1100

    router for registration and login completed

--- api/src/routers/UserRouter.js
+++ api/src/routers/UserRouter.js
@@ -1,19 +1,54 @@
-import express from 'express'
+import express from "express";
+import { createUser, getSingleUser } from "../model/user/UserModel.js";
 
-const router = express.Router()
+const router = express.Router();
 
 // register
 
-router.post("/", (req, res, next) => {
-    try {
-        console.log(req.body)
-        res.json({
-          status: "success",
-          message: "success",
-        });
-    } catch (error) {
-        next(error)
-    }
-  });
+router.post("/", async (req, res, next) => {
+  try {
+    console.log(req.body);
 
-  export default router;
+    const result = await createUser(req.body);
+
+    result?._id
+      ? res.json({
+          status: "success",
+          message: "Registration Successful",
+        })
+      : res.json({
+          status: "error",
+          message: "Unable to register user, Please try again later",
+        });
+  } catch (error) {
+    if (error.message.includes("E11000 duplicate key error collection")) {
+      (error.errorCode = 200),
+        (error.message =
+          "This email is already used, Please try with different email or reset with password");
+    }
+    next(error);
+  }
+});
+
+//login
+router.post("/login", async (req, res, next) => {
+  try {
+    console.log(req.body);
+
+    const result = await getSingleUserUser(req.body);
+
+    result?._id
+      ? res.json({
+          status: "success",
+          message: "Login Successful",
+        })
+      : res.json({
+          status: "error",
+          message: "Unable to login, Please try again later",
+        });
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;
